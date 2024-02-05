8bf23a2d07a98f9c199f6d2bcf195aa0c6b5b66a
Samira <samiraak@code.edu.az>
2024-02-05 15:36:30 +0400

    finish :white_check_mark:

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -3,10 +3,18 @@ import { Link } from 'react-router-dom'
 import Index from './Index'
 
 function About() {
+
+    const btn = {
+      padding: '10px',
+      backgroundColor: 'lavender',
+      border: 'none',
+      borderRadius: '40px'
+    };
   return (
-    <div className='container'>
+    <div className='container' style={{marginLeft: '50px', marginTop: '30px'}}>
       <div>Some info about me</div>
-      <Link to={'/'}> Back to Home </Link>
+      <br />
+      <button style={btn}><Link to={'/'}> Back to Home </Link></button>
     </div>
   )
 }
--- src/Pages/Index.jsx
+++ src/Pages/Index.jsx
@@ -2,61 +2,60 @@ import React, { useState } from 'react'
 import Style from '../Styles/Index.module.css'
 import { Input } from '@chakra-ui/react'
 import { SearchIcon } from '@chakra-ui/icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faWind } from '@fortawesome/free-solid-svg-icons'
 
 function Index() {
 
   const [input, setInput] = useState("");
-  const [weather, setWeather] = useState(null);
+  const [weather, setWeather] = useState({});
+  const [isError, setIsError] = useState(false);
 
-  const HandleSearchBtn = () => {
-      fetch(`https://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_API_KEY}&q=${input}&aqi=no`)
-    .then((res) => res.json())
-    .then((data) => {
+ const HandleSearchBtn = async() => {
+    if(!input.trim()) return
+    try{
+      const resp = await fetch(`https://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_API_KEY}&q=${input}&aqi=no`);
+      const data = await resp.json();
       const weatherData = {
-        location: `${data.location.name}`,
-        temperature: `${weather.current.temp_c} C`,
-        time: `${weather.location.localtime}`,
-        icon: `${weather.current.condition.icon}`,
-        wind_speed: `${weather.current.wind_mph} km/h`,
-        humidity: `${weather.current.humidity} %`,
-        feelslike_c: `${weather.current.feelslike_c} C`,
-        uv_index: `${weather.current.uv}`
+        location_city: `${data.location.name} /`,
+        location_country: `${data.location.country}`,
+        temperature: `${data.current.temp_c} C`,
+        time: `${data.location.localtime}`,
+        icon: `${data.current.condition.icon}`,
+        wind_speed: `${data.current.wind_mph} km/h`,
+        humidity: `${data.current.humidity} %`,
+        feelslike_c: `${data.current.feelslike_c} C`,
+        uv_index: `${data.current.uv}`
       }
       setWeather(weatherData);
-    })
-    .catch(err => console.log(err))
+      setIsError(false);
     }
-  
-  
-  const ErrorMessage = (msg) => {
-    const body = document.querySelector('.body');
-    body.innerHTML = `${msg}`
+    catch(err){
+      console.log(err);
+      setIsError(true);
+      setWeather({})
+    }
   }
-
 
   return (
     <div className={Style.main}>
       <div className={Style.header}>
-        <Input onChange={(e) => setInput(e.target.value)} padding={10} placeholder="Enter city name:" border={1} borderRadius={40}/>
+        <Input value={input} onChange={(e) => setInput(e.target.value)} padding={10} placeholder="Enter city name:" border={1} borderRadius={40}/>
         <button onClick={ HandleSearchBtn } className={Style.btn}><SearchIcon /></button>
       </div>
-      {weather && (
-        <div className={Style.body}>
-        <div className={Style.one}>{weather.location}</div>
-        <div className={Style.two}> <span className="time">{weather.time}</span></div>
-        <div className={Style.three}> <span className="date"></span></div>
-        <div className={Style.four}><img src='{weather.current.condition.icon}' alt=''/></div>
-        <div className={Style.five}> <span className="temperature">{weather.temperature}</span></div>
-        <div className={Style.footer}>
-          <div className="wind-speed"> {weather.wind_speed}</div>
-          <div className="humidity">{weather.humidity}</div>
-          <div className="feels-like">{weather.feelslike_c}</div>
-          <div className="uv-index">{weather.uv_index}</div>
-        </div>
-      </div>
-      )}
+      {isError && <div style={{color: 'red'}}>Location not found!</div>}
+      {weather ? 
+          <div className={Style.body}>
+            <div className={Style.one}>{weather.location_city} {weather.location_country}</div>
+            <div className={Style.two}> <span className="time">{weather.time}</span></div>
+            <div className={Style.four}><img src={`${weather.icon}`} alt=''/></div>
+            <div className={Style.five}>{weather.temperature}</div>
+            <div className={Style.footer}>
+              <div className="wind-speed"> {weather.wind_speed}</div>
+              <div className="humidity">{weather.humidity}</div>
+              <div className="feels-like">{weather.feelslike_c}</div>
+              <div className="uv-index">{weather.uv_index}</div>
+            </div>
+          </div>
+       : <div>{isError && <div style={{color: 'red'}}>Location not found!</div>} </div>}
     </div>
   )
 }
